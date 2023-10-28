b70a9a556d782b879c3fdf432542e989d4879c8c
tokhaihien <khaihienprivate@gmail.com>
2023-10-29 00:21:45 +0700

    update php admin

--- admin/login.php
+++
@@ -1,8 +0,0 @@
-<?php
-$level='';
-$page_tit='Đăng nhập hệ thống';
-$notSigned=true;
-$pro_path='process/login_process.php';
-$content_path='components/login_frm.php';
-require_once('layout.php');
-?>
---
+++ admin/statistics.php
@@ -0,0 +1,7 @@
+<?php
+$page_tit='Thống kê';
+$pro_path='process/statistics_pro.php';
+$content_path='components/statistics_content.php';
+$js='components/statistics_script.php';
+require_once('layout.php');
+?>
